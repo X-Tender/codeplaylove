4cbcdff86265bf6a20b9a6ddac7c837ee3c46fbb
Paul Kamma <paul.kamma@googlemail.com>
2018-05-27 10:06:17 +0200

    updated DynamicRoute
    replaced 'componentWIllMount' with 'componentDidMount' to fit then new react deprecations

--- src/jsx/Routes/DynamicRoute.jsx
+++ src/jsx/Routes/DynamicRoute.jsx
@@ -4,13 +4,15 @@ export default class DynamicRoute extends PureComponent {
 	state = {
 		component: null,
 	};
-	componentWillMount() {
+
+	componentDidMount() {
 		this.props.load().then(component => {
 			this.setState(() => ({
 				component: component.default ? component.default : component,
 			}));
 		});
 	}
+
 	render() {
 		return this.props.children(this.state.component);
 	}
