c345dd13233252477c90c02bb2ec1976604fad67
Paul Kamma <paul.kamma@googlemail.com>
2018-10-03 12:55:54 +0200

    Update about reducer
    removed deprecated stuff because it now own their own reducer

--- src/jsx/reducers/about.js
+++ src/jsx/reducers/about.js
@@ -1,19 +1,18 @@
 import HTTP from 'Utils/HTTP';
 
-const ABOUT_GET = 'about/GET';
+export const ABOUT_GET = 'about/GET';
 
 const initialState = {
 	loaded: false,
-	detailedIntroduction: {},
 };
 
 export default (state = initialState, action) => {
-	const { type, payload } = action;
+	const { type } = action;
+
 	switch (type) {
 		case ABOUT_GET:
 			return {
 				...state,
-				...{ detailedIntroduction: payload.detailedIntroduction },
 				loaded: true,
 			};
 
@@ -24,7 +23,6 @@ export default (state = initialState, action) => {
 
 export const getAbout = () => dispatch => {
 	HTTP.get('api/getAbout').then(response => {
-		console.log(response);
 		dispatch({
 			type: ABOUT_GET,
 			payload: response.data,
