8bf881543e932752ec4d4c10874a58482c3e05e8
Paul Kamma <paul.kamma@googlemail.com>
2018-10-03 09:44:56 +0200

    Removed NoWrap Component from DetailedIntroduction
    Not supported at the time till I get into the DIrectus RTE

--- src/jsx/Components/DetailedIntroduction.jsx
+++ src/jsx/Components/DetailedIntroduction.jsx
@@ -1,7 +1,6 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import convert from 'htmr';
-import NoWrap from 'Components/NoWrap';
 import Header from 'Components/Header';
 import Text from 'Components/Text';
 import P from 'Components/P';
