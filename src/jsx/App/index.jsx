efd08579b3f8e7f9531d7eff76306cf2fc5bd885
Paul Kamma <paul.kamma@googlemail.com>
2018-10-08 22:24:40 +0200

    Replaced static Game routes with dynamic API driven data

--- src/jsx/App/index.jsx
+++ src/jsx/App/index.jsx
@@ -6,14 +6,6 @@ import store, { history } from './store';
 
 import DynamicRoute from 'Routes/DynamicRoute';
 
-import CodenameGordon from 'Routes/Games/CodenameGordon';
-import ColdIron from 'Routes/Games/ColdIron';
-import IrishSushiSmuggler from 'Routes/Games/IrishSushiSmuggler';
-import MineMatcher from 'Routes/Games/MineMatcher';
-import StealthPervert from 'Routes/Games/StealthPervert';
-import Void from 'Routes/Games/Void';
-import ConelMan from 'Routes/Games/ConelMan';
-
 import Footer from 'Components/Footer';
 import Navigation from 'Components/Navigation';
 
@@ -53,6 +45,12 @@ const Privacy = props => (
 	</DynamicRoute>
 );
 
+const Game = props => (
+	<DynamicRoute load={() => import('./../Routes/Games/Game')}>
+		{Component => (Component === null ? null : <Component {...props} />)}
+	</DynamicRoute>
+);
+
 const App = () => (
 	<Provider store={store}>
 		<ConnectedRouter history={history}>
@@ -65,13 +63,7 @@ const App = () => (
 					<Route exact path="/games" component={Games} />
 					<Route exact path="/imprint" component={Imprint} />
 					<Route exact path="/privacy" component={Privacy} />
-					<Route exact path="/games/codename-gordon" component={CodenameGordon} />
-					<Route exact path="/games/cold-iron" component={ColdIron} />
-					<Route exact path="/games/irish-sushi-smuggler" component={IrishSushiSmuggler} />
-					<Route exact path="/games/mine-matcher" component={MineMatcher} />
-					<Route exact path="/games/stealth-pervert" component={StealthPervert} />
-					<Route exact path="/games/void" component={Void} />
-					<Route exact path="/games/conel-man" component={ConelMan} />
+					<Route path="/games/:gameSlug" component={Game} />
 				</main>
 				<Footer />
 			</React.Fragment>
---
+++ src/jsx/Routes/Games/Game.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import ResponsiveEmbed from 'react-responsive-embed';
+import convert from 'htmr';
+
+import { getGames } from 'reducers/games';
+import HeaderImage from 'Components/HeaderImage';
+import Header from 'Components/Header';
+import Text from 'Components/Text';
+import Article from 'Semantics/Article';
+import Section from 'Semantics/Section';
+import P from 'Components/P';
+import A from 'Components/A';
+
+const Video = ({ youtubeId, className, ratio = '4:3' }) => (
+	<div style={{ gridColumn: '1/-1' }} className={className}>
+		<ResponsiveEmbed
+			ratio={ratio}
+			src={`https://www.youtube-nocookie.com/embed/${youtubeId}?rel=0&amp;showinfo=0&theme=white`}
+			allowFullScreen
+		/>
+	</div>
+);
+
+const Images = ({ images }) => {
+	const imageItems = images.map(image => (
+		<img key={image} className="images-list__img" src={image} />
+	));
+	return <ul className={`images-list images-list--${images.length}`}>{imageItems}</ul>;
+};
+
+class Game extends React.Component {
+	constructor(props) {
+		super(props);
+		console.log('GAME');
+
+		if (!this.props.data.loaded) {
+			this.props.getGames();
+		}
+	}
+
+	render() {
+		console.log(this.props.data);
+		const { loaded, header, name, images, youtube, ratio, copy } = this.props.data;
+
+		if (!loaded) return null;
+
+		return (
+			<React.Fragment>
+				<HeaderImage src={header} pixelate />
+				<Section>
+					<Article>
+						<Header subhead="Game" headline={name} />
+						<div className="game-content clearfix">
+							<Video youtubeId={youtube} ratio={ratio} className="game-content__video" />
+							<Text className="game-content__text">
+								{convert(copy, {
+									transform: {
+										p: P,
+										a: A,
+									},
+								})}
+							</Text>
+						</div>
+					</Article>
+				</Section>
+
+				<Section fullWidth>
+					<Article isPage>
+						<Images images={images} />
+					</Article>
+				</Section>
+				<Section />
+			</React.Fragment>
+		);
+	}
+}
+
+const mapStateToProps = ({ games }, ownProps) => {
+	const { gameSlug } = ownProps.match.params;
+	return { data: { loaded: games.loaded, ...games.gameList.find(game => game.slug === gameSlug) } };
+};
+
+export default connect(
+	mapStateToProps,
+	{ getGames }
+)(Game);
