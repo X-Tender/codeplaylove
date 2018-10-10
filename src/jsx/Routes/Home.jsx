bd8d56c310006e668654c0826ffb16cde48acd02
Paul Kamma <paul.kamma@googlemail.com>
2018-10-10 23:19:49 +0200

    Refactored Home to stateless component
    - Removed gridload (what does it do here?)

--- src/jsx/Routes/Home.jsx
+++ src/jsx/Routes/Home.jsx
@@ -1,38 +1,14 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { getPosts } from 'reducers/grid';
-
-import GridItem from 'Components/GridItem';
 import Hero from 'Components/Hero';
 import Introduction from 'Components/Introduction';
 import CodePlayLoveCards from 'Components/CodePlayLoveCards';
 
-class Home extends React.PureComponent {
-	componentDidMount() {
-		if (this.props.grid.length === 0) this.props.getPosts();
-	}
+const Home = (
+	<React.Fragment>
+		<Hero />
+		<Introduction />
+		<CodePlayLoveCards />
+	</React.Fragment>
+);
 
-	getItems() {
-		return this.props.grid.map((item, index) => <GridItem key={index} data={item} />);
-	}
-
-	render() {
-		return (
-			<React.Fragment>
-				<Hero />
-				<Introduction />
-				<CodePlayLoveCards />
-			</React.Fragment>
-		);
-	}
-}
-
-const mapStateToProps = ({ grid }) => ({
-	grid: grid.data,
-});
-
-const mapDispatchToProps = {
-	getPosts,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+export default Home;
