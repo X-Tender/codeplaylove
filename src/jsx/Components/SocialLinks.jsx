2fae3fa7db9656a37896e02b53a3c3c3d165449b
Paul Kamma <paul.kamma@googlemail.com>
2018-10-15 21:59:19 +0200

    Refactored SocialLink Component
    Added sperate prop for icon to make it managable via direectus

--- src/jsx/Components/SocialLinks.jsx
+++ src/jsx/Components/SocialLinks.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const SocialLinkItem = ({ url, children }) => (
+const SocialLinkItem = ({ url, iconName, children }) => (
 	<li className="social-links__list-item">
 		<a
 			target="_blank"
@@ -10,18 +10,28 @@ const SocialLinkItem = ({ url, children }) => (
 			className="social-links__link"
 			title={children}
 		>
-			<i className={`fab fa-${children.toLowerCase()} fa-3x`} />
+			<i className={`fab fa-${iconName} fa-3x`} />
 		</a>
 	</li>
 );
 
 const SocialLinks = () => (
 	<ul className="social-links">
-		<SocialLinkItem url="//github.com/X-Tender">GitHub</SocialLinkItem>
-		<SocialLinkItem url="//twitter.com/xtender">Twitter</SocialLinkItem>
-		<SocialLinkItem url="//instagram.com/xtender">Instagram</SocialLinkItem>
-		<SocialLinkItem url="//facebook.com/paul.kamma">Facebook</SocialLinkItem>
-		<SocialLinkItem url="//xing.com/profile/Paul_Kamma">XING</SocialLinkItem>
+		<SocialLinkItem url="//github.com/X-Tender" iconName="github">
+			GitHub
+		</SocialLinkItem>
+		<SocialLinkItem url="//twitter.com/xtender" iconName="twitter">
+			Twitter
+		</SocialLinkItem>
+		<SocialLinkItem url="//instagram.com/xtender" iconName="instagram">
+			Instagram
+		</SocialLinkItem>
+		<SocialLinkItem url="//facebook.com/paul.kamma" iconName="facebook">
+			Facebook
+		</SocialLinkItem>
+		<SocialLinkItem url="//xing.com/profile/Paul_Kamma" iconName="xing">
+			XING
+		</SocialLinkItem>
 	</ul>
 );
 export default SocialLinks;
