5bec502ca8229aa0aeabb5fb418a85769ce160a7
Paul Kamma <paul.kamma@googlemail.com>
2018-10-24 22:13:20 +0200

    Added CodePlayLoveCard

---
+++ src/jsx/Components/CodePlayLoveCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+	title: PropTypes.string.isRequired,
+	children: PropTypes.node.isRequired,
+	style: PropTypes.string.isRequired,
+};
+
+const CodePlayLoveCard = ({ title, children, style }) => (
+	<li className={`code-play-love-card code-play-love-card--${style}`}>
+		<div className={`code-play-love-card__icon code-play-love-card__icon--${style}`} />
+		<div className="code-play-love-card__content">
+			<h3 className="code-play-love-card__head">{title}</h3>
+			{children}
+		</div>
+	</li>
+);
+
+CodePlayLoveCard.propTypes = propTypes;
+
+export default CodePlayLoveCard;
