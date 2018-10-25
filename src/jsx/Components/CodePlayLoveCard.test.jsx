d86e84602c64afef6a25e7051bf4bc8628e50b4f
Paul Kamma <paul.kamma@googlemail.com>
2018-10-25 21:29:18 +0200

    Update test for CodePlayLoveCard

--- src/jsx/Components/CodePlayLoveCard.test.jsx
+++ src/jsx/Components/CodePlayLoveCard.test.jsx
@@ -3,13 +3,23 @@ import { shallow } from 'enzyme';
 import CodePlayLoveCard from './CodePlayLoveCard';
 
 describe('CodePlayLoveCard', () => {
+	const style = 'code';
+	const title = 'Code';
+	const children = 'This is code';
+
 	const comp = shallow(
-		<CodePlayLoveCard style="code" title="Code">
-			<p>Lorem ipsum</p>
+		<CodePlayLoveCard style={style} title={title}>
+			{children}
 		</CodePlayLoveCard>
 	);
 
 	it('renders correctly', () => {
 		expect(comp).toMatchSnapshot();
+	});
+
+	it('renders the correct props', () => {
+		expect(comp.find(`li.code-play-love-card.code-play-love-card--${style}`).exists()).toBe(true);
+		expect(comp.find('h3.code-play-love-card__head').text()).toEqual(title);
+		expect(comp.find('div.code-play-love-card__content').text()).toEqual(title + children);
 	});
 });
