7ab526f23bf9efc76946749e45e6f07000f2f9e3
Paul Kamma <paul.kamma@googlemail.com>
2018-10-25 21:46:06 +0200

    Update test for Button

--- src/jsx/Components/Button.test.jsx
+++ src/jsx/Components/Button.test.jsx
@@ -3,9 +3,26 @@ import { shallow } from 'enzyme';
 import Button from './Button';
 
 describe('Button', () => {
-	const button = shallow(<Button to="/" />);
+	const to = '/target';
+	const children = 'The target';
+	const isPrimary = true;
+	const isGhost = true;
+	const isFullWithSm = true;
+
+	const buttonMinProps = shallow(<Button to={to} />);
+	const buttonAllProps = shallow(
+		<Button isFullWithSm={isFullWithSm} isGhost={isGhost} isPrimary={isPrimary} to={to}>
+			{children}
+		</Button>
+	);
 
 	it('renders correctly', () => {
-		expect(button).toMatchSnapshot();
+		expect(buttonMinProps).toMatchSnapshot();
+	});
+
+	it('renders the correct props', () => {
+		expect(
+			buttonAllProps.find('.button.button--primary.button--ghost.button--full-width-sm').exists()
+		).toBe(true);
 	});
 });
