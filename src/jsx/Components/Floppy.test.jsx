9e7b8647d747bd46a10b4f9a054258e870fa9af1
Paul Kamma <paul.kamma@googlemail.com>
2018-10-25 20:57:37 +0200

    Update test for Floppy

--- src/jsx/Components/Floppy.test.jsx
+++ src/jsx/Components/Floppy.test.jsx
@@ -3,13 +3,23 @@ import { shallow } from 'enzyme';
 import Floppy from './Floppy';
 
 describe('Floppy', () => {
+	const cover = 'uploads/image.jpg';
+	const to = '/my-game';
+	const children = 'My Game';
+
 	const comp = shallow(
-		<Floppy cover="uploads/image.jpg" to="/">
-			Test game
+		<Floppy cover={cover} to={to}>
+			{children}
 		</Floppy>
 	);
 
 	it('renders correctly', () => {
 		expect(comp).toMatchSnapshot();
+	});
+
+	it('renders the correct props', () => {
+		expect(comp.find('.floppy').prop('title')).toEqual(children);
+		expect(comp.find('.floppy').prop('to')).toEqual(to);
+		expect(comp.find('.floppy__cover').prop('src')).toEqual(cover);
 	});
 });
