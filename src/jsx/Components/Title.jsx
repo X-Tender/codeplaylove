bafd191987be878cfaa52ac1d35c264650b63e56
Paul Kamma <paul.kamma@googlemail.com>
2018-10-26 23:22:32 +0200

    Update Title

--- src/jsx/Components/Title.jsx
+++ src/jsx/Components/Title.jsx
@@ -1,30 +1,18 @@
 import React from 'react';
-import MaskedSVG from './MaskedSVG';
-
-const Code = () => (
-	<span className="title__text title__text--code">
-		<MaskedSVG color="#8ac926" text="code" type="code" />
-	</span>
-);
-
-const Play = () => (
-	<span className="title__text title__text--play">
-		<MaskedSVG color="#2e84d1" text="PLAY" type="play" />
-	</span>
-);
-
-const Love = () => (
-	<span className="title__text title__text--love">
-		<MaskedSVG color="#fc5053" text="love" type="love" />
-	</span>
-);
+import TitleSegment from './TitleSegment';
 
 const Title = () => (
 	<div className="title">
 		<h1 className="title__head">
-			<Code />
-			<Play />
-			<Love />
+			<TitleSegment color="#8ac926" type="code">
+				code
+			</TitleSegment>
+			<TitleSegment color="#2e84d1" type="play">
+				PLAY
+			</TitleSegment>
+			<TitleSegment color="#fc5053" type="love">
+				love
+			</TitleSegment>
 		</h1>
 	</div>
 );
