408b4b4bb838f4c769f8fc770cbc1ca039d68e42
Paul Kamma <paul.kamma@googlemail.com>
2018-10-26 23:06:21 +0200

    Update test for HeaderImage

--- src/jsx/Components/HeaderImage.test.jsx
+++ src/jsx/Components/HeaderImage.test.jsx
@@ -4,19 +4,39 @@ import HeaderImage from './HeaderImage';
 
 describe('HeaderImage', () => {
 	const src = 'image.jpg';
-	const isPixelated = true;
 	const caption = 'This is a caption';
 
-	const comp = shallow(<HeaderImage caption={caption} isPixelated={isPixelated} src={src} />);
+	describe('not pixelated', () => {
+		const notPixelated = shallow(<HeaderImage caption={caption} src={src} />);
 
-	it('renders correctly', () => {
-		expect(comp).toMatchSnapshot();
+		it('renders correctly', () => {
+			expect(notPixelated).toMatchSnapshot();
+		});
+
+		it('renders the correct props', () => {
+			expect(notPixelated.find('img.header-image__img').prop('src')).toEqual(src);
+			expect(notPixelated.find('img.header-image__img.header-image__img--pixelate').exists()).toBe(
+				false
+			);
+			expect(notPixelated.find('span.header-image__caption').exists()).toBe(true);
+			expect(notPixelated.find('span.header-image__caption').text()).toEqual(caption);
+		});
 	});
 
-	it('renders the correct props', () => {
-		expect(comp.find('img.header-image__img').prop('src')).toEqual(src);
-		expect(comp.find('img.header-image__img.header-image__img--pixelate').exists()).toBe(true);
-		expect(comp.find('span.header-image__caption').exists()).toBe(true);
-		expect(comp.find('span.header-image__caption').text()).toEqual(caption);
+	describe('pixelated', () => {
+		const pixelated = shallow(<HeaderImage caption={caption} isPixelated src={src} />);
+
+		it('renders correctly', () => {
+			expect(pixelated).toMatchSnapshot();
+		});
+
+		it('renders the correct props', () => {
+			expect(pixelated.find('img.header-image__img').prop('src')).toEqual(src);
+			expect(pixelated.find('img.header-image__img.header-image__img--pixelate').exists()).toBe(
+				true
+			);
+			expect(pixelated.find('span.header-image__caption').exists()).toBe(true);
+			expect(pixelated.find('span.header-image__caption').text()).toEqual(caption);
+		});
 	});
 });
