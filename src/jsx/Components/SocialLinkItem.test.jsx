bb3686b458275fa85a67ae5ab410ba02bf0259fc
Paul Kamma <paul.kamma@googlemail.com>
2018-10-26 20:23:09 +0200

    Added test for  SocialListItem

---
+++ src/jsx/Components/SocialLinkItem.test.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import SocialLinkItem from './SocialLinkItem';
+
+describe('SocialLinkItem', () => {
+	const url = 'https://github.com';
+	const iconName = 'github';
+	const children = 'gitHub';
+
+	const comp = shallow(
+		<SocialLinkItem iconName={iconName} url={url}>
+			{children}
+		</SocialLinkItem>
+	);
+
+	it('renders correctly', () => {
+		expect(comp).toMatchSnapshot();
+	});
+
+	it('renders correnct props', () => {
+		expect(comp.find('li a').prop('href')).toEqual(url);
+		expect(comp.find('li a').prop('name')).toEqual(children);
+		expect(comp.find('li a').prop('title')).toEqual(children);
+		expect(comp.find('li a i').hasClass(`fa-${iconName}`)).toBe(true);
+	});
+});
