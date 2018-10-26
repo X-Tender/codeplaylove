a98f16e7c0b0f86ab61b34a110f4b0f578fa8df2
Paul Kamma <paul.kamma@googlemail.com>
2018-10-26 23:06:05 +0200

    Update test for Header

--- src/jsx/Components/Header.test.jsx
+++ src/jsx/Components/Header.test.jsx
@@ -7,28 +7,33 @@ describe('Header', () => {
 	const headline = 'My headline';
 	const className = 'my-class';
 
-	const headerWithClass = shallow(
-		<Header className={className} headline={headline} subhead={subhead} />
-	);
-	const headerWithoutClass = shallow(<Header headline={headline} subhead={subhead} />);
+	describe('with class', () => {
+		const headerWithClass = shallow(
+			<Header className={className} headline={headline} subhead={subhead} />
+		);
 
-	it('renders correctly with class', () => {
-		expect(headerWithClass).toMatchSnapshot();
+		it('renders correctly', () => {
+			expect(headerWithClass).toMatchSnapshot();
+		});
+
+		it('renders the correct props', () => {
+			expect(headerWithClass.find(`header.${className}`).exists()).toBe(true);
+			expect(headerWithClass.find('.introduction__subhead').text()).toEqual(subhead);
+			expect(headerWithClass.find('.introduction__headline').text()).toEqual(headline);
+		});
 	});
 
-	it('renders correctly without class', () => {
-		expect(headerWithoutClass).toMatchSnapshot();
-	});
+	describe('without class', () => {
+		const headerWithoutClass = shallow(<Header headline={headline} subhead={subhead} />);
 
-	it('renders the correct props with class', () => {
-		expect(headerWithClass.find(`header.${className}`).exists()).toBe(true);
-		expect(headerWithClass.find('.introduction__subhead').text()).toEqual(subhead);
-		expect(headerWithClass.find('.introduction__headline').text()).toEqual(headline);
-	});
+		it('renders correctly', () => {
+			expect(headerWithoutClass).toMatchSnapshot();
+		});
 
-	it('renders the correct props without class', () => {
-		expect(headerWithoutClass.find('header').prop('className')).toEqual('header ');
-		expect(headerWithoutClass.find('.introduction__subhead').text()).toEqual(subhead);
-		expect(headerWithoutClass.find('.introduction__headline').text()).toEqual(headline);
+		it('renders the correct props', () => {
+			expect(headerWithoutClass.find('header').prop('className')).toEqual('header ');
+			expect(headerWithoutClass.find('.introduction__subhead').text()).toEqual(subhead);
+			expect(headerWithoutClass.find('.introduction__headline').text()).toEqual(headline);
+		});
 	});
 });
