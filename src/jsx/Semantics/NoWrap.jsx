e309d10fec8dc5bd4db92ad7637360b39af3afc2
Paul Kamma <paul.kamma@googlemail.com>
2018-10-29 21:13:56 +0100

    Update NoWrap propTypes

--- src/jsx/Semantics/NoWrap.jsx
+++ src/jsx/Semantics/NoWrap.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const propTypes = {
-	children: PropTypes.number.isRequired,
+	children: PropTypes.node.isRequired,
 };
 
 const NoWrap = ({ children }) => <span className="no-wrap">{children}</span>;
