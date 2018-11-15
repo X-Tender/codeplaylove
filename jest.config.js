730e4563396cf3852d0c6e2ff5e6ec1882b7d7e0
Paul Kamma <paul.kamma@googlemail.com>
2018-11-15 22:44:00 +0100

    Update jest.config.js
    Added various files to ignore testing

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-	collectCoverageFrom: ['<rootDir>/src/jsx/**/*.{js,jsx}', '!<rootDir>/src/jsx/reducers/index.js'],
+	collectCoverageFrom: [
+		'<rootDir>/src/jsx/**/*.{js,jsx}',
+		'!<rootDir>/src/jsx/reducers/index.js',
+		'!<rootDir>/src/jsx/index.jsx',
+		'!<rootDir>/src/jsx/App/store.js',
+	],
 	setupFiles: ['<rootDir>/cfg/jest/shim.js', '<rootDir>/cfg/jest/setup.js'],
 	testMatch: ['<rootDir>/src/jsx/**/?(*.)(spec|test).(js|jsx)'],
 	testEnvironment: 'jsdom',
