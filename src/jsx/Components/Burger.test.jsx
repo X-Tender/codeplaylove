8e8525a8b8e10f7c90b800d7830b6051b6929607
Paul Kamma <paul.kamma@googlemail.com>
2018-11-26 20:42:11 +0100

    Updated test for Burger

--- src/jsx/Components/Burger.test.jsx
+++ src/jsx/Components/Burger.test.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { Burger } from './Burger';
 
 describe('Burger', () => {
 	const toggleMenu = jest.fn();
-	const burger = shallow(<Burger toggleMenu={toggleMenu} />);
+	let burger = shallow(<Burger toggleMenu={toggleMenu} />);
 
 	it('renders correctly', () => {
 		expect(burger).toMatchSnapshot();
+	});
+
+	burger = shallow(<Burger isOpen toggleMenu={toggleMenu} />);
+	it('renders correctly when isOpen prop is given', () => {
+		expect(burger.find('button').hasClass('isOpen')).toBe(true);
+	});
+
+	describe('when mounted', () => {
+		beforeEach(() => {
+			const props = {
+				isOpen: false,
+				toggleMenu,
+			};
+			burger = mount(<Burger {...props} />);
+		});
+
+		it('dispatches the `toggleMenu()` method when menuButton was clicked', () => {
+			burger.find('button').simulate('click');
+			expect(toggleMenu).toHaveBeenCalled();
+		});
 	});
 });
