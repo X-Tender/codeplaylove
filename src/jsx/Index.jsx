05878d40eef6c709d62151c35a9e4b6b63545dcb
Paul Kamma <paul.kamma@googlemail.com>
2019-08-29 22:16:29 +0200

    Added possibliy to dsiable react dev tools
    But keep it disabled for demo purpose

--- src/jsx/Index.jsx
+++ src/jsx/Index.jsx
@@ -2,7 +2,21 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from 'App';
 
-require('disable-react-devtools');
+if (process.env.NODE_ENV === 'production') {
+	/*
+	I disable the deactivation of the devtools.
+	I mean when I make the source public it is useless
+	to disable the devtools, this way any currious dev
+	can peek at the stuff 😀
+	*/
+	/*
+	if (typeof window.__REACT_DEVTOOLS_GLOBAL_HOOK__ === 'object') {
+		for (const [key, value] of Object.entries(window.__REACT_DEVTOOLS_GLOBAL_HOOK__)) {
+			window.__REACT_DEVTOOLS_GLOBAL_HOOK__[key] = typeof value === 'function' ? () => {} : null;
+		}
+	}
+	*/
+}
 
 if (module.hot) {
 	module.hot.accept();
