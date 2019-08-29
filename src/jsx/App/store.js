4af664542a8e7e25ec9b1370815a6efc14332ff7
Paul Kamma <paul.kamma@googlemail.com>
2019-08-29 22:15:46 +0200

    Updated redux devtools integration
    But keep it enabled to show my store as demo purpose

--- src/jsx/App/store.js
+++ src/jsx/App/store.js
@@ -1,5 +1,6 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { routerMiddleware } from 'react-router-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import createHistory from 'history/createBrowserHistory';
 
@@ -7,21 +8,22 @@ import reducer from 'reducers';
 
 export const history = createHistory();
 
-const enhancers = [];
 const middleware = [thunk, routerMiddleware(history)];
 
-if (process.env.NODE_ENV !== 'development') {
-	const devToolsExtension = window.devToolsExtension;
+/*
+This way I would prevet that the redux tools are available in production mode.
+I disable this because I would like to show it, also the source is availbale anyway
+*/
 
-	if (typeof devToolsExtension === 'function') {
-		enhancers.push(devToolsExtension());
-	}
-}
+/*
+const devTools =
+	process.env.NODE_ENV === 'production'
+		? applyMiddleware(...middleware)
+		: composeWithDevTools(applyMiddleware(...middleware));
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+	const store = createStore(reducer, devTools);
+*/
 
-const enhancer = composeEnhancers(applyMiddleware(...middleware));
-
-const store = createStore(reducer, enhancer);
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(...middleware)));
 
 export default store;
